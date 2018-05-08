885ee5d3d42452888262a2994ebef89e0fe3a04d
huqiming <huqiming@navercorp.com>
2018-05-08 15:57:11 +0800

    client

--- client.js
+++ client.js
@@ -16,8 +16,12 @@ const fileStream = fs.createWriteStream(filePath);
 
 const client = new net.Socket();
 
+let byteCount = 0;
+let startTS = null;
+
 client.on('data', (data)=>{
-    console.log('......');
+    // console.log(`${data.byteLength} is received`);
+    byteCount = byteCount + data.byteLength;
     fileStream.write(data);
 });
 
@@ -33,3 +37,11 @@ client.connect({
     port : serverInfo.port,
     host : serverInfo.hostname
 });
+
+startTS = Date.now();
+
+setInterval(()=>{
+    let ts = Date.now();
+    let rate = byteCount/1024/((ts - startTS)/1000);
+    console.info(`${(new Date()).toString()}: ${(byteCount/1024/1024).toFixed(2)}MB is reseived, with rate ${rate.toFixed(2)}KB/s`)
+}, 10 * 1000)
